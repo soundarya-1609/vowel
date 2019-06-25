ed8d464b26d7ae19f123c6c5828acf844c735336
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-06-25 06:42:35 -0700

    Update consonentvow.py

--- consonentvow.py
+++ consonentvow.py
@@ -1,8 +1,8 @@
 char=input("Enter the character:")
 if char.isalpha():
     if(char=='A'or char=='E'or char=='I'or char=='O'or char=='U'or char=='a'or char=='e'or char=='i'or char=='o'or char=='u'):
-          print(char,"is a vowel")
+          print("Vowel")
     else:
-          print(char,"is a consonent")
+          print("Consonant")
 else:
           print("invalid")
