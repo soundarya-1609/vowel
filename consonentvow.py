9b09251365bf6fc6e9d24caa66f327041b5f81a5
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-06-25 15:46:07 +0530

    Create consonentvow.py

---
+++ consonentvow.py
@@ -0,0 +1,5 @@
+char=input("Enter the character:")
+if(char='A',char='E',char='I',char='O',char='U',char='a',char='e',char='i',char='o',char='u'):
+      print(char,"is a vowel")
+else:
+      print(char,"is a consonent")
